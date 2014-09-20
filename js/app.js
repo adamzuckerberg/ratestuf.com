dee62851e8b0fa845f560998bfc3d3deea5ba7c2
adamzuckerberg <adam@spagroups.com>
2014-09-19 22:55:28 -0700

    working on jQuery ajax post image to server

--- js/app.js
+++ js/app.js
@@ -113,9 +113,9 @@ $( ".draggable" ).parent().css( "background-color", "20px red" );
                           $("#loginFacebook").css('borderRadius','10px');
                           $("#loginFacebook").css('margin-top','22px');
                       });
-                      $("#rateNowButton").click(function() {
-                          $("#loginFacebook").css('border', 'none');
-                      });
+                      // $("#rateNowButton").click(function() {
+                      //     $("#loginFacebook").css('border', 'none');
+                      // });
                       $("#rateNowButton").mouseout(function() {
                           $("#loginFacebook").css('border', 'none');
                           $("#loginFacebook").css('margin-top','40px');
@@ -135,9 +135,11 @@ $( ".draggable" ).parent().css( "background-color", "20px red" );
 // ********************************************************************
 // INSERT RATINGS INTO DATABASE AND CREATE HTML CANVAS DYNAMICALLY
 // ********************************************************************
+
   var data ={};
   data.items = [];
 
+  canvasBalls = [];
 $("#rateNowButton").click(function(){
 
   if ($(this).hasClass('disabled')) {
@@ -148,6 +150,9 @@ $("#rateNowButton").click(function(){
    alert("Please search for an item first.");
     return;
   }
+
+// prevent multiple clicks due to user ADHD. Part 1 of 2. Re-enabled later on in code
+  $(this).addClass('disabled');
 
   $('.draggable').each(function() {
 
@@ -172,211 +177,164 @@ $("#rateNowButton").click(function(){
 
   data.items.push({"name": itemName, "itemId": itemId, "xAxis": xAxis, "xRating":xRating, "yAxis":yAxis, "yRating":yRating});
 
-  window.console && console.log(data.items);
-  window.console && console.log(data);
-
 // ********************************************************
 // Dynamically Create HTHML5 Mirror of Rating Table Section
 // ********************************************************
 
-          // var comparison = [{
-          //     "name": data.items.itemName,
-          //     "itemId": data.items[itemId],
-          //     "searchTerm": "Obama vs. Batman",
-          //     "xAxis": data.items.xAxis,
-          //     "xRating": data.items.xRating,
-          //     "yAxis": data.items.yAxis,
-          //     "yRating": data.items.yRating,
-          //     "color": '#009900'
-          //   },
-          //     {
-          //     "name": "batman",
-          //     "itemId": 123,
-          //     "searchTerm": "Obama vs. Batman",
-          //     "xAxis": "coolness",
-          //     "xRating": 0.85,
-          //     "yAxis": "intelligence",
-          //     "yRating": 0.4,
-          //     "color": '#009900'
-          //   },
-          //     {
-          //       "name": "obama",
-          //       "itemId": 123,
-          //       "searchTerm": "Obama vs. Batman",
-          //       "xAxis": "coolness",
-          //       "xRating": 0.5,
-          //       "yAxis": "intelligence",
-          //       "yRating": 0.1,
-          //       "color": '#33ccff'
-          //     }];
+  actualContainerHeight = $(this).parent().height();
+  actualContainerWidth = $(this).parent().width();
+  xRatingCanvas = (Math.round((positionFromLeft / actualContainerWidth) * 100 )/ 100);
+  yRatingCanvas = (Math.round((1-(positionFromTop / actualContainerHeight))* 100 )/ 100);
 
-          // var canvas = $("canvas")[0];
-          // canvas.width = 1000;
-          // canvas.height = 533;
+// IMPORTANT - Data source and HTML Canvas measure from top left. Data sent to db is for humans which measure Cartesian from bottom left
+  canvasBalls.push({"name": itemName, "itemId": itemId, "xAxis": xAxis, "xRating": xRatingCanvas, "yAxis":yAxis, "yRating": yRatingCanvas});
 
-          // var ctx = canvas.getContext("2d");
-          // var centerX = $("canvas").width() / 2;
-          // var centerY = $("canvas").height() / 2;
-          // var radius = 25;
-          // var canvas = $("canvas")[0];
+          var canvas = $("canvas")[0];
+          canvas.width = 1000;
+          canvas.height = 533;
 
-          // // Draw Background
-          // ctx.beginPath();
-          // ctx.rect(70, 4, canvas.width-70-4, canvas.height-63);
-          // // ctx.fillStyle = "#CFBE6E";
-          // ctx.fillStyle = "rgba(207, 190, 110, 0.3)";
-          // ctx.fill();
-          // ctx.lineWidth = 4;
-          // ctx.strokeStyle = '#000';
-          // ctx.stroke();
+          var ctx = canvas.getContext("2d");
+          var centerX = $("canvas").width() / 2;
+          var centerY = $("canvas").height() / 2;
+          var radius = 25;
+          var canvas = $("canvas")[0];
 
-          // // // Arrow up/down
-          // ctx.beginPath();
-          // ctx.fillStyle = "#999";
-          // ctx.fillRect(25, 50, 20,
-          //   canvas.height  - 110);
-          // ctx.lineWidth = 2;
-          // ctx.strokeStyle = '#000';
-          // ctx.stroke();
+          // Draw Background
+          ctx.beginPath();
+          ctx.rect(70, 4, canvas.width-70-4, canvas.height-63);
+          // ctx.fillStyle = "#CFBE6E";
+          ctx.fillStyle = "rgba(207, 190, 110, 0.3)";
+          ctx.fill();
+          ctx.lineWidth = 4;
+          ctx.strokeStyle = '#000';
+          ctx.stroke();
 
-          // ctx.fillStyle = "#999";
+          // // Arrow up/down
+          ctx.beginPath();
+          ctx.fillStyle = "#999";
+          ctx.fillRect(25, 50, 20,
+            canvas.height  - 110);
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = '#000';
+          ctx.stroke();
 
-          // ctx.moveTo(35, 0);
-          // ctx.lineTo(10, 50);
-          // ctx.lineTo(60, 50);
-          // ctx.fill();
+          ctx.fillStyle = "#999";
+
+          ctx.moveTo(35, 0);
+          ctx.lineTo(10, 50);
+          ctx.lineTo(60, 50);
+          ctx.fill();
 
 
-          // // // Arrow left/right
-          // ctx.fillStyle = "#999";
-          // ctx.fillRect(
-          //   70, // x
-          //   canvas.height - 40, // y
-          //   canvas.width - 130,
-          //   20
-          // );
-          // ctx.fillStyle = "#999";
-          // ctx.beginPath();
-          // ctx.moveTo(1000, 505); //point
-          // ctx.lineTo(940, 480);
-          // ctx.lineTo(940, 525);
-          // ctx.fill();
+          // // Arrow left/right
+          ctx.fillStyle = "#999";
+          ctx.fillRect(
+            70, // x
+            canvas.height - 40, // y
+            canvas.width - 130,
+            20
+          );
+          ctx.fillStyle = "#999";
+          ctx.beginPath();
+          ctx.moveTo(1000, 505); //point
+          ctx.lineTo(940, 480);
+          ctx.lineTo(940, 525);
+          ctx.fill();
 
 
-          // // // Input x-axis
-          // ctx.beginPath();
-          // ctx.rect(  380, // x
-          //   canvas.height - 50, // y
-          //   300,
-          //   45
-          // );
-          // ctx.fillStyle = "white";
-          // ctx.fill();
-          // ctx.lineWidth = 2;
-          // ctx.strokeStyle = 'grey';
-          // ctx.stroke();
+          // // Input x-axis
+          ctx.beginPath();
+          ctx.rect(  380, // x
+            canvas.height - 50, // y
+            300,
+            45
+          );
+          ctx.fillStyle = "white";
+          ctx.fill();
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = 'grey';
+          ctx.stroke();
 
-          // // // Input y-axis
-          // ctx.beginPath();
-          // ctx.rect(
-          //   0, // x
-          //   220, // y
-          //   300,
-          //   45
-          // );
-          // ctx.fillStyle = "white";
-          // ctx.fill();
-          // ctx.lineWidth = 2;
-          // ctx.strokeStyle = 'grey';
-          // ctx.stroke();
+          // // Input y-axis
+          ctx.beginPath();
+          ctx.rect(
+            0, // x
+            220, // y
+            300,
+            45
+          );
+          ctx.fillStyle = "white";
+          ctx.fill();
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = 'grey';
+          ctx.stroke();
 
-          // // // Draw ratings and balls in a foreach loop - can we split this up to design the balls with shadow etc.
-          // comparison.forEach(function (data) {
-          //   ctx.beginPath();
-          //   ctx.arc(canvas.width * data.xRating,
-          //           canvas.height * data.yRating,
-          //           radius, 0, 2 * Math.PI, false);
-          //   ctx.fillStyle = data.color;
-          //   ctx.fill();
-          //   ctx.lineWidth = 4;
-          //   ctx.strokeStyle = '#003300';
-          //   ctx.stroke();
-          //   ctx.fill();
-
-          //   ctx.fillStyle = "black";
-          //   ctx.textAlign = 'center';
-          //   ctx.font = "normal 800 32px Arial";
-
-          //   ctx.fillText(
-          //     data.name,
-          //     canvas.width * data.xRating,
-          //     canvas.height * data.yRating + 55
-          //   );
-          // });
+                    // // X legend
+          ctx.fillStyle = "black";
+          ctx.textAlign = 'center';
+          ctx.font = "36px Arial";
+          ctx.fillText(
+            // the xaxis name will be the same for ball 2 if it exists
+            canvasBalls[0].xAxis,
+            canvas.width / 2 + 27,
+            canvas.height - 17
+          );
 
 
-          // // // X legend
-          // ctx.fillStyle = "black";
-          // ctx.textAlign = 'center';
-          // ctx.font = "36px Arial";
-          // ctx.fillText(
-          //   comparison[0].xAxis,
-          //   canvas.width / 2 + 27,
-          //   canvas.height - 17
-          // );
+          // // Y legend
+          ctx.fillStyle = "black";
+          ctx.textAlign = 'center';
+          ctx.font = "36px Arial";
+          ctx.fillText(
+            // the yaxis name will be the same for ball 2 if it exists
+            canvasBalls[0].yAxis,
+            152,
+            254
+          );
 
+          //Draw the first ball
+          ctx.beginPath();
+          ctx.fillStyle = "green";
+          ctx.arc((canvas.width * (1-(60/1000))) * canvasBalls[0].xRating,
+                    (canvas.height * (1-(53/533))) * (1-canvasBalls[0].yRating),
+                    radius, 0, 2 * Math.PI, false);
+          ctx.fill();
+          ctx.lineWidth = 4;
+          ctx.strokeStyle = '#003300';
+          ctx.stroke();
+          ctx.fill();
 
-          // // // Y legend
-          // ctx.fillStyle = "black";
-          // ctx.textAlign = 'center';
-          // ctx.font = "36px Arial";
-          // ctx.fillText(
-          //   comparison[0].yAxis,
-          //   152,
-          //   254
-          // );
+          ctx.fillStyle = "black";
+          ctx.textAlign = 'center';
+          ctx.font = "normal 800 32px Arial";
+          ctx.fillText(
+            canvasBalls[0].name,
+            (canvas.width * (1-(70/1000))) * canvasBalls[0].xRating + 20,
+            (canvas.height * (1-(63/533))) * (1-canvasBalls[0].yRating) + 55
+          );
 
-          // document.getElementById('result').src = data_url;
-          // console.log(data_url);
+  var png_image_source = $('#myCanvas')[0].toDataURL( 'image/png' );
+  console.log( png_image_source );
 
-
-          //   var data_url = canvas.toDataURL();
-
-          // $.post( "ajax/upload_data.php.html", function( data_url ) {
-          //   console.log("Payload to server" + data_url);
-          // }).done(function( data ) {
-          //   alert( "Response from server " + data );
-          // });
-
-// FROM VIDEO SEND CANVAS AS URL TO SERVER
-
-// var myDrawing = $("#myCanvas");
-// var drawingString = myDrawing.toDataURL("image/png");
-// var postData = "canvasData="+drawingString;
-// var ajax = new XMLHttpRequest();
-// ajax.open("POST",'postcanvastofb.php',true);
-// ajax.setRequestHeader('Content-Type', 'canvas/upload');
-// ajax.onreadystatechange=function()
-// {
-//   if (ajax.readyState == 4)
-//     {alert("image saved");}
-// }
-// ajax.send(postData);
-
-
+  $.post( "ajax/upload_canvas_image.php", {
+            image_source: png_image_source 
+          }).done(function( data ) {
+            alert( "Response from server " + data );
+          });
 
 
 // ********************************************************
 // Dynamically Create HTHML5 Mirror of Rating Table Section
 // ********************************************************
-
+// re-enable to button which was disabled to keep duplicate pushes of data into the array
+  $(this).addClass('enabled');
 });
 
        $.ajax({ 
 
+        data: JSON.stringify(data),
         type: "POST",
         url: "ajax/saveratings.php",
-        data: JSON.stringify(data),
         contentType: "application/json",
 
         success: function(res) {
@@ -388,14 +346,14 @@ $("#rateNowButton").click(function(){
                 $.ajax({ 
 
               type: "GET",
-              url: "ajax/saveratings.php",
-              url: "screenshot.php?s="+encodeURI($("#searchTags").val()),
+              url: "ajax/upload_canvas_image.php",
+              // url: "screenshot.php?s="+encodeURI($("#searchTags").val()),
               contentType: "application/json",
 
               success: function(res) {
                 // console.log(res);
 
-              window.open("https://www.facebook.com/dialog/feed?app_id=228744763916305&display=popup&caption=test&link=http://www.ratestuf.org"&$file,'_parent');
+              window.open("https://www.facebook.com/dialog/feed?app_id=228744763916305&display=popup&caption=test&link=http://www.ratestuf.org/&",'_parent');
 
 // ratestuf.org?i="+res.imageName+"&redirect_uri=https://www.facebook.com",ratestuf.org&redirect_uri=https://www.facebook.com",'_parent');
               },
@@ -419,8 +377,7 @@ $("#rateNowButton").click(function(){
         }
         ,dataType:'json'});
 
-      });
-
+ });  // end ratenowbutton click event
 
 
 
